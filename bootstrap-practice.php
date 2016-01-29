01386d02101db4011296e66391bd258dab6eb49b
Domkratos <dmartinez337@cnm.edu>
2016-01-29 09:31:21 -0700

    ...

---
+++ bootstrap-practice.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Bootstrap practice</title>
+		<?php require_once(dirname(__DIR__) . "/php/lib/head-tags.html"); ?>
+	</head>
+	<body>
+		<div class="container">
+			  <div class="row">
+				  <div class="col-md-12">
+					  <div class="jumbotron">
+						  <h1>Jumbotron, y'all!</h1>
+						  <p>Random ness in the jumbotronzz</p>
+					  </div>
+				  </div>
+			  </div>
+		</div>
+	</body>
+</html>
