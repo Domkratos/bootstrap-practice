83858dbbf162a26ed6f06c16814dbe4867a54cc6
Domkratos <dmartinez337@cnm.edu>
2016-02-02 10:02:06 -0700

    added in css style sheet

--- bootstrap-practice.php
+++ bootstrap-practice.php
@@ -3,18 +3,87 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Bootstrap practice</title>
-		<?php require_once(dirname(__DIR__) . "/php/lib/head-tags.html"); ?>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!-- Bootstrap Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous" />
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous" />
+		<link rel="stylesheet" href=//css/style.css />
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 	</head>
-	<body>
-		<div class="container">
-			  <div class="row">
-				  <div class="col-md-12">
-					  <div class="jumbotron">
-						  <h1>Jumbotron, y'all!</h1>
-						  <p>Random ness in the jumbotronzz</p>
-					  </div>
-				  </div>
-			  </div>
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<header>
+				<div class="container">
+				head
+				</div
+			</header>
+			<main>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="jumbotron">
+								jumbotron neow!
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-6">
+								blah blah blah
+							</div>
+							<div class="col-md-6">
+								blah blah blah 2
+							</div>
+						</div>
+
+					</div>
+					<div class="row">
+						<div class="col-md-3">
+							<div class="content-box">
+								<p>
+									small box 1
+								</p>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="content-box">
+								<p>
+									small box 2
+								</p>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="content-box">
+								<p>
+									small box 3
+								</p>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="content-box">
+								<p>
+									small box 4
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
 		</div>
+
 	</body>
+		<div class="container">
+ 		<footer>
+				footer here
+		</footer
 </html>
